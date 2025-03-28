d8733bfc2caf3775a26b33756a78c4d1a5e1c0c5
ozgurbaykalinitial <obaykal@initialcode.com.tr>
2025-03-28 18:45:32 +0300

    - add new 1.0.1 version of PensaSDK, edit the init function according to the new method, add new listeners

--- PublicRepoApp/PublicRepoAppApp.swift
+++ PublicRepoApp/PublicRepoAppApp.swift
@@ -16,10 +16,17 @@ struct PublicRepoAppApp: App {
         let clientId = Bundle.main.object(forInfoDictionaryKey: "CLIENT_ID") as! String
         let clientSecret = Bundle.main.object(forInfoDictionaryKey: "CLIENT_SECRET") as! String
         
-        Pensa.shared.enableLogging()
-        Pensa.shared.initPensa(clientId: clientId, clientSecret: clientSecret) { error in
-            if let error {
-                print("Error on initializing Pensa SDK: \(error)")
+        let config = PensaConfiguration.Builder()
+            .setClientId(clientId)
+            .setClientSecret(clientSecret)
+            .setLoggingEnabled(true)
+            .setScanUploadListener(self)
+            .setCantScanEventListener(self)
+            .build()
+        
+        Pensa.shared.initPensa(config: config) { error in
+            if let error = error {
+                print("Error initializing Pensa SDK: \(error)")
             } else {
                 print("Pensa SDK is ready to start!!")
             }
@@ -30,5 +37,23 @@ struct PublicRepoAppApp: App {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+extension PublicRepoAppApp: ScanUploadListener, CantScanEventListener {
+    func onScanUploadProgressUpdate(tdlinxId: String, scanAreaId: String, progress: Double) {
+        print("onScanUploadProgressUpdate() -> tdlinxId: \(tdlinxId), scanAreaId: \(scanAreaId) process: \(progress)")
+    }
+    
+    func onScanUploadCompleted(tdlinxId: String, scanAreaId: String) {
+            print("onScanUploadCompleted() -> Upload completed successfully tdlinxId: \(tdlinxId), scanAreaId: \(scanAreaId)")
+    }
+    
+    func onScanUploadFailed(tdlinxId: String, scanAreaId: String, error: CallbackError) {
+        print("onScanUploadFailed() -> Upload failed for tdlinxId: \(tdlinxId), scanAreaId: \(scanAreaId) Error Code: \(error.code.rawValue), Message: \(error.message)")
+    }
+    
+    func onCantScanReported(tdlinxId: String, scanAreaId: String, cantScanReason: String) {
+        print("onCantScanReported() -> tdlinxId: \(tdlinxId), scanAreaId: \(scanAreaId) cantScanReason: \(cantScanReason)")
     }
 }
